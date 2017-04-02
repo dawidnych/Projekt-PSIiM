7cbb08cb11e08d7f7ae2cfa64ec4ea2a0a8c8120
wstawinski <stawinskiwojciech@gmail.com>
2017-04-02 13:05:59 +0200

    added business client MyOfferts view

---
+++ LastMinuteWebApp/LastMinuteWebApp/Controllers/ClientBusinessController.cs
@@ -0,0 +1,25 @@
+﻿using LastMinuteWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastMinuteWebApp.Controllers
+{
+    public class ClientBusinessController : Controller
+    {
+        GrouponDBEntities2 DBOffert = new GrouponDBEntities2();
+
+        public ActionResult MyOfferts()
+        {
+            // ToDo: currently logged in client business id
+            int constIDClientBusiness = 1;
+
+            var myOfferts = (from o in DBOffert.Offert select o)
+                .Where(o => o.idClientBusiness == constIDClientBusiness);
+
+            return View(myOfferts);
+        }
+    }
+}
