e35407c44c35076b7d60d60a474ae39a45bf0e83
Kacper <kacperpilkowksi@gmail.com>
2017-05-17 17:51:00 +0200

    done

--- LastMinuteWebApp/LastMinuteWebApp/Controllers/AccountController.cs
+++ LastMinuteWebApp/LastMinuteWebApp/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
