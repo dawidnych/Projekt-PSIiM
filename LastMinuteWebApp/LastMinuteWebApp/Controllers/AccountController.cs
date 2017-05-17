e5e24565c3bbad3c37b06123043d94cb7e19256b
Kacper <kacperpilkowksi@gmail.com>
2017-05-17 03:19:01 +0200

    working with mob users

--- LastMinuteWebApp/LastMinuteWebApp/Controllers/AccountController.cs
+++ LastMinuteWebApp/LastMinuteWebApp/Controllers/AccountController.cs
@@ -72,10 +72,10 @@ namespace LastMinuteWebApp.Controllers
             {
                 return View(model);
             }
-
+            var user = await UserManager.FindByEmailAsync(model.Email);
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
